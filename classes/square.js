4a55cf06996b0e60cba1a8a2aff237d1f7edd3b2
Flu3nt <gnedyalkov94@gmail.com>
2023-06-09 15:26:14 +0300

    bugfix: square class is now called square.js

---
+++ classes/square.js
@@ -0,0 +1,15 @@
+class Square {
+  constructor(squareSide) {
+    this.side = squareSide;
+  }
+
+  area() {
+    return this.side * this.side;
+  }
+
+  static getbrand() {
+    return "Rubik's Square";
+  }
+
+  static brand = "Rubik's cube";
+}
