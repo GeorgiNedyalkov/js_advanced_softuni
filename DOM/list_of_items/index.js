96221e520dd9878a78f5b29b0850382ed7069c77
Flu3nt <gnedyalkov94@gmail.com>
2023-06-25 07:35:19 +0300

    Solve two problems

--- DOM/list_of_items/index.js
+++ DOM/list_of_items/index.js
@@ -11,3 +11,23 @@ function addItem() {
   inputEl.value = "";
   itemsList.appendChild(liEl);
 }
+
+function deleteRedElements() {
+  const redElements = document.querySelectorAll("#items-list li.red");
+  redElements.forEach((li) => li.parentElement.removeChild(li));
+}
+
+function deleteByEmail() {
+  const tableBody = document.querySelectorAll("#customers tbody")[0];
+  const inputEl = document.getElementsByName("email")[0];
+
+  document.querySelectorAll("table tr").forEach((tr) => {
+    const tdEmailElText = tr.children[1].textContent;
+
+    if (inputEl.value === tdEmailElText) {
+      tableBody.removeChild(tr);
+    }
+  });
+
+  inputEl.value = "";
+}
